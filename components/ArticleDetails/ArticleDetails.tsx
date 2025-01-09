b67603ec24c5c5993bc93d51c60a91b30934bf5d
Viktor Stefanov <victorystefanov@gmail.com>
2025-01-09 13:52:28 +0200

    article details view

--- components/ArticleDetails/ArticleDetails.tsx
+++ components/ArticleDetails/ArticleDetails.tsx
@@ -1,36 +1,44 @@
-import { View, Text, StyleSheet, StatusBar, Image, SafeAreaView, Dimensions } from "react-native";
+import { View, Text, StyleSheet, StatusBar, Image, SafeAreaView, Dimensions, ScrollView, ActivityIndicator } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import { ApolloProvider, useQuery } from "@apollo/client";
 import { GET_ARTICLE_BY_ID } from "@/services/queries";
 import client from "@/services/apolloClient";
 
+import { Article } from '@/constants/Article';
+import { useFonts } from "expo-font";
+
 const ArticleDetails = () => {
   const { id } = useLocalSearchParams();
-
   const articleId = parseInt(id as string, 10);
-  
-  const { loading, error, data } = useQuery(GET_ARTICLE_BY_ID, {
-    variables: { id: articleId  },
-  });
-  
-  
-    if (loading) return <Text>Loading articles...</Text>;
-    if (error) return <Text>Error: {error.message}</Text>;
 
-    const article = data.articles_by_pk;
+  const { loading, error, data } = useQuery(GET_ARTICLE_BY_ID, {
+    variables: { id: articleId },
+  });
+
+  const [fontsLoaded] = useFonts({
+    Inter: require("../../assets/fonts/Inter-Regular.ttf"),
+  });
+
+  if (loading) return <Text>Loading articles...</Text>;
+  if (error) return <Text>Error: {error.message}</Text>;
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
+  const article: Article = data.articles_by_pk;
 
   return (
-    <>
-      <StatusBar
-        translucent
-        barStyle={"light-content"}
-        backgroundColor="transparent"
-      />
-      <View style={styles.container}>
+      <ScrollView 
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <StatusBar translucent barStyle={"light-content"} backgroundColor="transparent" />
         <Image source={{ uri: article.image_url }} style={styles.articleImage} />
-      </View>
-    </>
+        <Text style={styles.articleTitle}>{article.title}</Text>
+        <Text style={styles.description}>{article.body}</Text>
+      </ScrollView>
   );
 };
 
@@ -47,14 +55,37 @@ const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 0, 
-    justifyContent: 'flex-start', 
+    marginTop: 0,
+  },
+  contentContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 20,
   },
   articleImage: {
-    width: width,  
-    height: 478,   
-    borderRadius: 0,  
-    resizeMode: 'cover', 
+    width: width,
+    height: 478,
+    borderRadius: 0,
+    resizeMode: 'cover',
+  },
+  articleTitle: {
+    fontSize: 32,
+    color: '#000',
+    width: 327,
+    textAlign: 'center',
+    fontWeight: "bold",
+    fontFamily: 'Inter',
+    marginTop: 20,
+  },
+  description: {
+    color: '#000',
+    width: 327,
+    fontFamily: 'Inter',
+    fontSize: 14,
+    lineHeight: 20,
+    textAlign: 'center',
+    marginTop: 10, 
+    paddingBottom: 20, 
   },
 });
 
