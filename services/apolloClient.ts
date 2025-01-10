17de223f058b77269302c1ae39a4d3ea7ca23e91
Viktor Stefanov <victorystefanov@gmail.com>
2025-01-10 02:24:04 +0200

    revert old setup bugfix with env

--- services/apolloClient.ts
+++ services/apolloClient.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { API_URL, API_KEY } from "@env";
+// import { API_URL, API_KEY } from "@env";
 
-// const API_URL = 'https://brave-blowfish-66.hasura.app/v1/graphql';
-// const API_KEY = 's2StHw61j1PoAOS55C5DPj20WBg6DLnMiJen5vmN8xwdap6tWS6QIVlS3zriuUtd';
+const API_URL = 'https://brave-blowfish-66.hasura.app/v1/graphql';
+const API_KEY = 's2StHw61j1PoAOS55C5DPj20WBg6DLnMiJen5vmN8xwdap6tWS6QIVlS3zriuUtd';
 
 const client = new ApolloClient({
   uri: API_URL,
